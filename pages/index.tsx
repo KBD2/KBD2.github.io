6eb69b9ff0e3d26fcc5745d8e879dc09ecb7bc0b
KBD2 <44832995+KBD2@users.noreply.github.com>
2023-01-07 21:35:45 +1300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>KBD2</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <p className="font-Inter">Sup</p>
+        </div>
+      </main>
+    </>
+  )
+}
