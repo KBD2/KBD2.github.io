d5ba2158ce960e657d6e03776fe9cfa702cd24ac
KBD2 <44832995+KBD2@users.noreply.github.com>
2023-01-07 21:51:25 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'kbd2.imgix.net'
+  }
+}
